32d0985bf3c6413f54fd4e90eecfc9e08fa1964d
Puyantao <18503970627@163.com>
2023-04-26 10:28:10 +0800

    1

--- Httplibrary/src/main/java/com/example/mylibrary/net/retrofit/interceptor/ReportInterceptor.java
+++ Httplibrary/src/main/java/com/example/mylibrary/net/retrofit/interceptor/ReportInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.mylibrary.net.retrofit.interceptor;
 
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.io.IOException;
@@ -20,13 +21,13 @@ public class ReportInterceptor implements Interceptor {
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        Long startTime = System.currentTimeMillis();
+        Long startTime = SystemClock.uptimeMillis();
         Request request = chain.request();
         HttpUrl url = request.url();
 
         Response response = chain.proceed(request);
         int code = response.code();
-        Long endTime = System.currentTimeMillis();
+        Long endTime = SystemClock.uptimeMillis();
         Log.i("----->1", endTime - startTime + "");
         return response;
     }
