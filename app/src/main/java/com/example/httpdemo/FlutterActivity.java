ca9a6aa42d16a8ea10cdfa150b648da05b7bad4b
puyantao@xurongwangluo.com <PyT@123759>
2025-04-03 21:28:26 +0800

    # 参数配置

---
+++ app/src/main/java/com/example/httpdemo/FlutterActivity.java
@@ -0,0 +1,15 @@
+package com.example.httpdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FlutterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flutter);
+    }
+}
