e31f228991b38747d168c2cb917678bbd6c5616f
puyantao@xurongwangluo.com <PyT@123759>
2025-04-23 15:42:54 +0800

    # 参数配置

--- app/src/main/java/com/example/httpdemo/MyApplication.java
+++ app/src/main/java/com/example/httpdemo/MyApplication.java
@@ -3,12 +3,9 @@ package com.example.httpdemo;
 import android.app.Application;
 import android.content.Context;
 
-import com.example.httpdemo.helper.PushConstants;
 import com.example.httpdemo.untils.HttpUtil;
-import com.example.httpdemo.helper.UmPushHelper;
 import com.example.mylibrary.net.http.OkHttpProcessor;
 import com.example.mylibrary.untils.Utils;
-import com.umeng.commonsdk.UMConfigure;
 
 
 /**
@@ -28,21 +25,6 @@ public class MyApplication extends Application {
 //        Utils.init(this, new RetrofitProcessor(this), HttpUtil.getMainHost());
 //        Utils.init(this, new VolleyProcessor(this), HttpUtil.getMainHost());
         Utils.init(this, new OkHttpProcessor(this), HttpUtil.getMainHost());
-
-        //友盟日志输出开关
-        UMConfigure.setLogEnabled(true);
-        //预初始化
-        UMConfigure.preInit(this, PushConstants.APP_KEY, PushConstants.CHANNEL);
-        // todo 添加是否同意隐私政策
-
-        //建议在子线程中初始化
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // 初始化友盟
-                UmPushHelper.init(getApplicationContext());
-            }
-        }).start();
 
     }
 
